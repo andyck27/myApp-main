c2aa468328fcb6f5a2709b8ecc45d6863b4ea651
Andy Kim <78758139+andyck27@users.noreply.github.com>
2024-04-14 14:31:25 -0700

    fix #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CS6440 Lab 4 - SMART on FHIR';
+  title = 'Nutrition';
 }
