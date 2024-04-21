138316a8d6a5faba2bce90e23c3829f1a32279e6
Andy Kim <78758139+andyck27@users.noreply.github.com>
2024-04-21 14:10:06 -0700

    Health monitor warning implementation

--- src/app/components/view-nutrition-list/view-nutrition-list.component.ts
+++ src/app/components/view-nutrition-list/view-nutrition-list.component.ts
@@ -9,6 +9,15 @@ import { Router } from '@angular/router';
 })
 export class ViewNutritionListComponent {
   formDataArray: any[] = [];
+  totalCalories: number = 0;
+  totalCarbohydrates: number = 0;
+  totalProtein: number = 0;
+  totalFat: number = 0;
+  isAtRiskOfDiabetes: boolean = false;
+  calorieThreshold: number = 2000;
+  carbohydrateThreshold: number = 600;
+  isAtRiskOfHeartAttack: boolean = false;
+  fatThreshold: number = 70;
 
   constructor(
     private nutritionDataService: NutritionDataService,
@@ -16,10 +25,36 @@ export class ViewNutritionListComponent {
 
   ngOnInit(): void {
     this.formDataArray = this.nutritionDataService.formDataArray;
+    this.calculateTotals();
+    this.checkDiabetesRisk();
+    this.checkHeartAttackRisk();
   }
 
   editNutrition(nutrition: any) {
     this.router.navigate(['/enter-nutrition'], { queryParams: { editId: nutrition.id }});
+  }
+
+  calculateTotals(): void {
+    this.totalCalories = this.formDataArray.reduce((total, nutrition) => total + nutrition.calories, 0);
+    this.totalCarbohydrates = this.formDataArray.reduce((total, nutrition) => total + nutrition.carbohydrates, 0);
+    this.totalProtein = this.formDataArray.reduce((total, nutrition) => total + nutrition.protein, 0);
+    this.totalFat = this.formDataArray.reduce((total, nutrition) => total + nutrition.fat, 0);
+  }
+
+  checkDiabetesRisk(): void {
+    if (this.totalCalories > this.calorieThreshold || this.totalCarbohydrates > this.carbohydrateThreshold) {
+      this.isAtRiskOfDiabetes = true;
+    } else {
+      this.isAtRiskOfDiabetes = false;
+    }
+  }
+
+  checkHeartAttackRisk(): void {
+    if (this.totalFat > this.fatThreshold) {
+      this.isAtRiskOfHeartAttack = true;
+    } else {
+      this.isAtRiskOfHeartAttack = false;
+    }
   }
 
   closeList() {
